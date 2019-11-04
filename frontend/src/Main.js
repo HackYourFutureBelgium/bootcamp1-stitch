0cf2af9a92b589eba9f52a1e1e9130230e540b50
Alfi Yusrina <alfi.yusrina@hotmail.com>
2019-11-04 14:30:43 +0100

    setting up a boilerplate, consist of Header, Footer, and Main

---
+++ frontend/src/Main.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  componentDidMount() {
+
+  }
+
+
+  render() {
+    return (
+      <main className="content">
+        <h1>How does it work?</h1>
+        <p>someone has to make a body text here, Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+      </main>
+    );
+  }
+}
+
+export default Main;
