a8cbc281494e21bb8b5f6ebe68c5ec5a26bd9a06
Alfi Yusrina <alfi.yusrina@hotmail.com>
2019-11-04 16:21:58 +0100

    seperating signupSection

---
+++ frontend/src/SignUpSection.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const SignUpSection = () => {
+  return (
+    <section className="signupSection">
+      <h1>Interested?Join us...</h1>
+      <button>Sign up now</button>
+    </section>
+  );
+
+};
+
+
+export default SignUpSection;
