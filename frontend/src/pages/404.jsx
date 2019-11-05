5a62ae3b8346d3aa405066916d7a6287ac8eef9c
mediamonster <asashvili@gmail.com>
2019-11-05 19:29:26 +0100

    add react components and install materiali-ui

---
+++ frontend/src/pages/404.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const NotFoundPage = () => (
+  <div>
+    <h1>Page does not exist</h1>
+    <p>
+      You&apos;ve stumbled on a page that doesn&apos;t exist. <br />
+      If you believe a page should be here, please contact us at contact@hackyourfuture.be.
+    </p>
+    {/* <Link to="/">Back to main page</Link>*/}
+  </div>
+);
+
+export default NotFoundPage;
