5a9b326046def46141be12b265478f64873202ba
Alfi Yusrina <alfi.yusrina@hotmail.com>
2019-11-06 13:17:17 +0100

    positioning header with justify content space between

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Input, Button } from 'antd';
 
 class Header extends Component {
   constructor(props) {
@@ -16,6 +17,7 @@ class Header extends Component {
     return (
       <header className="app-header">
         <h1 className="app-header__title">Stitch</h1>
+        <Button type="primary" icon="login">Log in</Button>
       </header>
     );
   }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,5 +4,6 @@ import App from './App';
 
 import 'normalize.css';
 import './styles/index.css';
+import 'antd/dist/antd.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
