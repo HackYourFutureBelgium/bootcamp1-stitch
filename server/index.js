b273ab55a09a2813a65465694f85524fc2bad816
prabhamanju <manjuparbha@gmail.com>
2019-11-07 11:14:43 +0100

    DB connection established and created routes

--- server/index.js
+++ server/index.js
@@ -1,15 +1,17 @@
+require('dotenv').config();
 const express = require ('express');
 const bodyParse = require ('body-parser');
 const routes = require('./routes/route');
-const mongoose = require ('mongoose');
+//const mongoose = require ('mongoose');
 
 
 const app = express();
 const port = 4000;
 
+app.use(bodyParse.json());
 
 app.use('/', routes);
 
-app.listen(4000, function(){
+app.listen(port, function(){
     console.log('server running at ' +port);
 });
---
+++ server/routes/route.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require ('mongoose');
+
+const { DB_URI } = process.env;
+mongoose.connect(DB_URI , {useNewUrlParser: true ,  useUnifiedTopology: true })
+.then(() => {
+    console.log("connection established");
+}
+)
+.catch((err) => {
+    console.error(err);
+});
+
+const userProfile = new mongoose.Schema({
+    name: {type: String, default: "NULL"},
+    email: {type: String, default: "NULL"}
+    })
+    
+const profile = mongoose.model("profile", userProfile);
+
+
+router.get('/', (req, res) => {
+    res.send('HOME PAGE');
+});
+
+router.get('/search', (req, res) => {
+
+    profile.find().then(xyz => res.send(xyz) );
+    
+    
+});
+
+router.post('/signup', (req, res) => {
+    //res.send('signup page');
+     profile.create(req.body)
+    .then(profilecreated => res.send(profilecreated))
+    .catch((err) => {
+        console.error(err);
+    });
+});
+
+router.get('/connection', (req, res) => {
+    res.send('connection_info');
+
+});
+router.put('/post', (req, res) => {
+    res.send('added to you timeline');
+
+});
+router.delete('/deletepost', (req, res) => {
+    res.send('deleted post');
+
+});
+
+/*router.delete('/notification', (req, res) => {
+    res.send('deleted post');
+
+});*/
+
+router.put('/update', (req, res) => {
+    res.send('added to you timeline');
+
+});
+
+
+module.exports = router;
