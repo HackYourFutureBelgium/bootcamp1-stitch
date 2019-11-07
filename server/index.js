7328ac6a5c4ca6dac823304e47d99359af6efbed
Nihan2019 <fnakkose@hotmail.com>
2019-11-07 13:15:54 +0100

    DB connection controlled

--- server/index.js
+++ server/index.js
@@ -1,27 +1,28 @@
 require('dotenv').config();
 const express = require('express');
-const bodyParse = require('body-parser');
+const bodyParser = require('body-parser');
 const routes = require('./routes/route');
-//const mongoose = require ('mongoose');
+const cors = require('cors');
+//const mongoose = require('mongoose');
 
 
 const app = express();
 const port = 4000;
 
-app.use(bodyParse.json());
+app.use(bodyParser.json());
 
 app.use('/', routes);
 app.use(cors());
 
-const uri = process.env.ATLAS_URI;
-console.log(uri);
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log("MongoDB database connection established successfully");
-  })
-  .catch((err) => {
-    console.log(error)
-  });
+// const uri = process.env.DB_URI;
+// console.log(uri);
+// mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
+//   .then(() => {
+//     console.log("MongoDB database connection established successfully");
+//   })
+//   .catch((err) => {
+//     console.log(error)
+//   });
 
 
 app.listen(port, () => {
--- server/model/test.user.js
+++ server/model/test.user.js
@@ -1,18 +1,18 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-const Schema = mongoose.Schema;
+// const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-    minlength: 3
-  },
-}, {
-  timestamps: true,
-});
+// const userSchema = new Schema({
+//   name: {
+//     type: String,
+//     required: true,
+//     unique: true,
+//     trim: true,
+//     minlength: 3
+//   },
+// }, {
+//   timestamps: true,
+// });
 
-const User = mongoose.model('User', userSchema);
-module.exports = User;
+// const User = mongoose.model('User', userSchema);
+// module.exports = User;
