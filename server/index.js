79235112b6f795e1efc87dd28b3b8f430ae4f9f7
Nihan2019 <fnakkose@hotmail.com>
2019-11-07 11:21:04 +0100

    DB connection established

--- server/index.js
+++ server/index.js
@@ -1,15 +1,30 @@
-const express = require ('express');
-const bodyParse = require ('body-parser');
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
 const routes = require('./routes/route');
-const mongoose = require ('mongoose');
 
 
 const app = express();
-const port = 4000;
-app.use(bodyParse.json());
+
+const port = process.env.PORT || 4000;
 
 app.use('/', routes);
+app.use(cors());
 
-app.listen(4000, function(){
-    console.log('server running at ' +port);
+const uri = process.env.ATLAS_URI;
+console.log(uri);
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("MongoDB database connection established successfully");
+  })
+  .catch((err) => {
+    console.log(error)
+  });
+
+
+app.listen(port, () => {
+  console.log('server running at ' + port);
 });
--- server/routes/route.js
+++ server/routes/route.js
@@ -1,12 +1,42 @@
 const express = require('express');
 const router = express.Router();
+const bodyParser = require('body-parser');
 
-router.get('/', function(req, res){
-    res.send('hello world');
+
+const user = [];
+
+router.use(bodyParser.json());
+
+
+router.get('/', function (req, res) {
+  res.send('Home Page');
 });
 
-/*router.get('/signup', function(req, res){
-    res.send('signup page');
-});*/
+router.get('/signup', function (req, res) {
+  res.send('signup page');
+});
+
+router.post('/signup', function (req, res) {
+  user.push(res.body);
+  res.send(res.body);
+  console.log(req.body);
+});
+
+
+// router.get('/login', function (req, res) {
+//   res.send('login page');
+// });
+// router.get('/profilewithskill', function (req, res) {
+//   res.send('profile with skill page');
+// });
+// router.get('/connections', function (req, res) {
+//   res.send('connection page');
+// });
+// router.get('/signin', function (req, res) {
+//   res.send('sign in page');
+// });
+// router.get('/profilewithskilladdtimeline', function (req, res) {
+//   res.send('profile with skill add time line page');
+// });
 
 module.exports = router;
