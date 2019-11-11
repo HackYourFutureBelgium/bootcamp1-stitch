a99619d97a5919b6239e5d40b68e65b582ef5538
Alfi Yusrina <alfi.yusrina@hotmail.com>
2019-11-11 19:34:42 +0100

    added illustration on the introduction:

--- frontend/src/components/Introduction.js
+++ frontend/src/components/Introduction.js
@@ -3,8 +3,11 @@ import React from 'react';
 const Introduction = () => {
   return (
     <section className="introduction">
-      <h1>Your companion for a lifelong learning journey</h1>
-      <p>How often the world recognizing an individual by her/his formal degree? And what happens with your informal learning journey like attending informal events, writing an article, and so on? Isn't that great if you can show off the worlds both recognitions all in one place? Stitch can help you with that.</p>
+      <img className="introduction-img" src="stitching.png" alt="Stitching your future" title="Stich your experience" />
+      <div className="introduction-text">
+        <h1>Your companion for your learning journey</h1>
+        <p>How often the world recognizing an individual by her/his formal degree? And what happens with your informal learning journey like attending informal events, writing an article, and so on? Isn't that great if you can show off the worlds both recognitions all in one place? Stitch can help you with that.</p>
+      </div>
     </section>
   );
 
