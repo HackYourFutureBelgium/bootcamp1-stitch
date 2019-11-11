0abacf89b222318570923d5f52158089e1904429
Alfi Yusrina <alfi.yusrina@hotmail.com>
2019-11-11 21:26:43 +0100

    added howtogetstarted explaination

---
+++ frontend/src/components/HowToGetStarted.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const HowToGetStarted = () => {
+  return (
+    <section className="app-HowToGetStarted">
+      <h1>How To Get Started</h1>
+      <ul>
+        <li>Fill your <span>timeline</span> based on your skills</li>
+        <li>Share your <span>profile link</span> to people you like</li>
+        <li>Get Connected and <span>get stiched<span></li>
+      </ul>
+    </section>
+  );
+
+};
+
+
+export default HowToGetStarted;
--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Introduction from './Introduction';
+import HowToGetStarted from './HowToGetStarted';
 import SignUpSection from './SignUpSection';
 import ProfileFinder from './ProfileFinder';
 
@@ -20,6 +21,7 @@ class Main extends Component {
     return (
       <main className="content">
         <Introduction />
+        <HowToGetStarted />
         <SignUpSection />
         <ProfileFinder />
 
