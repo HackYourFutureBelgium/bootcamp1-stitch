6894d18e7cfe73dd6511b13b2912cb00469837bc
Alfi Yusrina <alfi.yusrina@hotmail.com>
2019-11-13 00:41:53 +0100

    added the functionality on search bar

--- frontend/src/components/ProfileFinder.js
+++ frontend/src/components/ProfileFinder.js
@@ -1,23 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Input } from 'antd';
 
-
-
-const ProfileFinder = () => {
-  const { Search } = Input;
-  return (
-    <section className="app-profileFinder">
-      <h1 className="app-profileFinder__title">Find a Stitcher profile</h1>
-      <p>If you have a Stitcher referrential link already, you can paste it here</p>
-      <Search
-      placeholder="input the referrential link here"
-      icon="search"
-      enterButton="Find"
-      size="large"
-      onSearch={value => console.log(value)}
-    />
-    </section>
-  );
+class ProfileFinder extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value : '',
+    };
+  }
+  handleChange = (e) => {
+   this.setState({value: e.currentTarget.value});
+  }
+  handleSearch = () => {
+    window.open(this.state.value);
+    this.setState({value:''});
+  }
+  render(){
+    const { Search } = Input;
+    return (
+      <section className="app-profileFinder">
+        <h1 className="app-profileFinder__title">Find a Stitcher profile</h1>
+        <p>If you have a Stitcher referrential link already, you can paste it here</p>
+        <Search
+        placeholder="input the referrential link here"
+        value={this.state.value}
+        onChange={this.handleChange}
+        icon="search"
+        enterButton="Find"
+        size="large"
+        onSearch={this.handleSearch}
+      />
+      </section>
+    );
+  }
 
 };
 
