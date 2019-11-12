3c04d5b16ad10418857244f8a30ff47d6beec77b
Nihan2019 <fnakkose@hotmail.com>
2019-11-12 13:52:36 +0100

    updated server side

---
+++ server/dev-server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const testingApp = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('./config');
+
+mongoose.Promise = global.Promise;
+mongoose
+  .connect(process.env.MONGODB_URL, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
+  .then(() => { console.log('Database connection established'); })
+  .catch((err) => {
+    console.error(`Database error, exiting. Stack trace:\n${err}`);
+    process.exit();
+  });
+
+
+testingApp.use(cors());
+testingApp.use(bodyParser.urlencoded({ extended: true }));
+testingApp.use(bodyParser.json());
+
+testingApp.get('/', (req, res) => {
+  res.json({ message: 'the frontend would serve at this route, the api is in the /api route' });
+});
+
+testingApp.get('/api', (req, res) => {
+  res.json({ message: 'API ready' });
+});
+
+const apiServices = require("./services");
+testingApp.use("/api", apiServices);
+
+const port = process.env.PORT || 4000;
+testingApp.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
---
+++ server/posts/dev-server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+require('../config'); // assign environmental variables
+
+mongoose.Promise = global.Promise;
+mongoose
+  .connect(process.env.MONGODB_URL, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
+  .then(() => { console.log('Database connection established'); })
+  .catch((err) => {
+    console.error(`Database error, exiting. Stack trace:\n${err}`);
+    process.exit();
+  });
+
+const testingApp = express();
+
+testingApp.use(cors());
+testingApp.use(bodyParser.urlencoded({ extended: true }));
+testingApp.use(bodyParser.json());
+
+testingApp.get('/', (req, res) => {
+  res.json({ welcome: 'you are testing the posts api' });
+});
+
+const posts = require("./routes");
+testingApp.use("/", posts);
+
+const port = process.env.PORT || 4000;
+testingApp.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
