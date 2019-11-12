209050c4145371225a63a302d608556e1f05a522
Alfi Yusrina <alfi.yusrina@hotmail.com>
2019-11-12 10:15:05 +0100

    added img getconnected

--- frontend/src/components/HowToGetStarted.js
+++ frontend/src/components/HowToGetStarted.js
@@ -3,11 +3,11 @@ import React from 'react';
 const HowToGetStarted = () => {
   return (
     <section className="app-HowToGetStarted">
-      <h1>How To Get Started</h1>
-      <ul>
-        <li>Fill your <span>timeline</span> based on your skills</li>
-        <li>Share your <span>profile link</span> to people you like</li>
-        <li>Get Connected and <span>get stiched<span></li>
+      <h1 className="app-HowToGetStarted__title">How To Get Started</h1>
+      <ul className="app-HowToGetStarted__list">
+        <li className="app-HowToGetStarted__item">Fill your <span>timeline</span> based on your skills</li>
+        <li className="app-HowToGetStarted__item">Share your <span>profile link</span> to people you like</li>
+        <li className="app-HowToGetStarted__item">Get Connected and <span>get stiched</span></li>
       </ul>
     </section>
   );
