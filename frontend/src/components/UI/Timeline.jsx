4b2528029e1d9d7eeda12d947b4adebdf337d1b3
mediamonster <asashvili@gmail.com>
2019-11-13 10:36:45 +0100

    remove react-bootstrap

--- frontend/src/components/UI/Timeline.jsx
+++ frontend/src/components/UI/Timeline.jsx
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
-import { Timeline, Icon, Avatar, Card } from 'antd';
+import { Timeline, Icon, Avatar, Card, Button } from 'antd';
 import '../../styles/style.css';
 import '../../styles/styleProfile.css';
-import Button from './Button';
 
 const { Meta } = Card;
 
@@ -15,38 +14,34 @@ export default class Timeline1 extends Component {
   constructor(props) {
     super(props);
   }
+
   render() {
     return (
       <div>
         <Timeline mode="alternate">
-          <Timeline.Item color="purple">
-            <h2>{this.props.details.url}</h2>
-            <p>{this.props.details.description}</p>
-            <h3>{this.props.details.skill}</h3>
+          <Timeline.Item color="black" dot={<Icon type="github" style={{ fontSize: '20px' }} />}>
+            <h1>{this.props.details.time}</h1>
+
             <Card
-              style={{ cardStyle }}
-              cover={
-                <img
-                  alt="example"
-                  src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
-                />
-              }
+              hoverable
+              style={cardStyle}
               actions={[
-                <Icon type="setting" key="setting" />,
-                <Icon type="edit" key="edit" />,
-                <Icon type="ellipsis" key="ellipsis" />
+                <Icon type="github" key="setting" />,
+                <p target="_blanck">{this.props.details.url}</p>
               ]}
             >
-              <h1>{this.props.details.title}</h1>
               <Meta
                 avatar={
                   <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
                 }
                 title="Card title"
-                description="This is the description"
               />
+              <h3>{this.props.details.skill}</h3>
+              <h2>{this.props.details.title}</h2>
+              <p>{this.props.details.description}</p>
             </Card>
           </Timeline.Item>
+
           <Timeline.Item color="red" dot={<Icon type="radar-chart" />}>
             <p>Solve initial network problems 2015-09-01</p>
           </Timeline.Item>
@@ -71,14 +66,7 @@ export default class Timeline1 extends Component {
           >
             Technical testing 2015-09-01 5555555
           </Timeline.Item>
-          <Timeline.Item color="yellow">Create a services site 2015-09-01</Timeline.Item>
-          <Timeline.Item color="green">Solve initial network problems 2015-09-01</Timeline.Item>
-          <Timeline.Item dot={<Icon type="facebook" theme="filled" style={{ fontSize: '30px' }} />}>
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
-            laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi
-            architecto beatae vitae dicta sunt explicabo.
-          </Timeline.Item>
-          <hr></hr>
+
           <hr></hr>
           <Button style={{ marginLeft: 500, whiteSpace: 'nowrap' }}>
             <a href="http://localhost:3000/profile">Go Back</a>
