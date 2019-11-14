c75095490921c086394956ea49be3e0657afeeec
abdulsalamalmahdi <getmeips@gmail.com>
2019-11-14 15:53:40 +0100

    n

--- server/index.js
+++ server/index.js
@@ -2,7 +2,9 @@ const express = require ('express');
 const app = express();
 const PORT = 4000;
 
-
+app.get('/', (req, res) => {
+    res.send("wuck findows");
+})
 
 app.listen(PORT, () => {
     console.log (`listening on port ${PORT}`);
