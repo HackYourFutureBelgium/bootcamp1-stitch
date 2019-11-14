9ef7be6abda297cc44a2afde03fd28cad80aea61
TamarAsashvili <47082054+TamarAsashvili@users.noreply.github.com>
2019-11-14 10:27:52 +0100

    Update PersonDetail.jsx

--- frontend/src/components/PersonDetail.jsx
+++ frontend/src/components/PersonDetail.jsx
@@ -1,85 +1,76 @@
 import React from 'react';
 import Timeline1 from '../components/UI/Timeline';
-import { Row, Col, Icon, Popconfirm, message, Button, Avatar, Card } from 'antd';
+import { Row, Col, Icon, Button, Card } from 'antd';
 import AddPost from './UI/AddPost';
 import '../styles/styleProfile.css';
-
-const text = 'Are you sure to delete this task?';
-
-function confirm() {
-  message.info('Clicked on Yes.');
-}
 
 class PersonDetail extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      skill: '',
-      title: '',
-      description: '',
-      url: '',
-      time: ''
+      posts: [
+        {
+          skills: '',
+          title: '',
+          description: '',
+          url: '',
+          time: ''
+        }
+      ]
     };
   }
 
   handleSubmit = async value => {
+    const { posts } = this.state;
     const { skill, title, description, url, time } = value;
-
-    await this.setState({
+    const newPost = {
       skill,
       title,
       url,
       description,
       time
-    });
+    };
+
+    // TODO Before setting the new post to state, make API request and wait for successful response
+    posts.push(newPost);
+    await this.setState({ posts });
     console.log(this.state);
+  };
+
+  //delete todo
+  delTodo = url => {
+    this.setState({ posts: [...this.state.posts.filter(post => post.url !== url)] });
   };
 
   render() {
     return (
       <div>
         <Row>
-          <Col span={7} hoverable style={{ border: '2px solid blue' }}>
-            <container id="prof_img">
-              <Avatar size={154} icon="user" alt="profimg" />
-              <p>
-                ipsum dolor sit amet consectetur adipisicing elit. Inventore similique obcaecati
-                aliquam quam illo fuga molestias doloribus ipsam, recusandae ullam magni cupiditate
-                corporis corrupti delectus exercitationem odit?
-              </p>
-              <Card
-                actions={[
-                  <Icon type="facebook" key="setting" />,
-                  <Icon type="github" key="edit" />,
-                  <Icon type="radar-chart" key="edit" />,
-                  <Avatar src="https://picsum.photos/id/8/200/200" />
-                ]}
-              ></Card>
-            </container>
+          <Col span={7} style={{ border: '2px solid blue' }}>
+            <p>
+              ipsum dolor sit amet consectetur adipisicing elit. Inventore similique obcaecati
+              aliquam quam illo fuga molestias doloribus ipsam, recusandae ullam magni cupiditate
+              corporis corrupti delectus exercitationem odit?
+            </p>
+            <Card
+              actions={[
+                <Icon type='facebook' key='setting' />,
+                <Icon type='github' key='edit' />,
+                <Icon type='radar-chart' key='edit' />
+              ]}
+            ></Card>
           </Col>
-          <Col span={2}>
+          <Col span={7}>
             <Button>
-              <Icon type="search" />
+              <Icon type='search' />
               Filter by skils
             </Button>
-
-            <Popconfirm
-              placement="bottom"
-              title={text}
-              onConfirm={confirm}
-              okText="Yes"
-              cancelText="No"
-            >
-              <Button style={{ fontSize: '20px' }}>Delete</Button>
-            </Popconfirm>
           </Col>
-          <Col span={15} style={{ padding: '40px' }}>
+          <Col span={10} style={{ padding: '40px' }}>
             <AddPost onSubmit={this.handleSubmit} />
-            <Timeline1 details={this.state} />
+            <Timeline1 details={this.state.posts} delTodo={this.delTodo} />
           </Col>
         </Row>
-        <hr />
-        <hr />
       </div>
     );
   }
