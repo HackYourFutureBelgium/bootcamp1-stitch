812ddb467ec6f83cbac72fcd06777f5e02f4dd24
Alfi Yusrina <alfi.yusrina@hotmail.com>
2019-11-14 01:03:44 +0100

    removing dark theme in the header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,49 @@
 import React, { Component } from 'react';
+import HeaderBeforeLogIn from './components/HeaderBeforeLogIn';
+import HeaderAfterLogIn from './components/HeaderAfterLogIn';
 import Routes from './routes/routes';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Context from './Context';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 class App extends Component {
-  state = {};
+  constructor(props) {
+  super(props);
+  const user = localStorage.getItem('user');
+  if(user === null){
+    this.state = {
+      user: {
+        email: '',
+        name: ''
+      }
+    };
+  }else{
+    this.state = {
+      user: JSON.parse(user),
+    };
+  }
+
+}
+setAuthenticatedUser = user =>{
+  localStorage.setItem('user', JSON.stringify(user));
+  this.setState({ user });
+};
+
   render() {
+    const context = {
+     user: this.state.user,
+     setAuthenticatedUser: this.setAuthenticatedUser
+   }
     return (
         <div className="App">
-          <Routes />
-          <Footer />
+          <Context.Provider value={context}>
+            <Router>
+              <HeaderBeforeLogIn />
+              <Routes />
+              <Footer />
+            </Router>
+          </Context.Provider>
         </div>
     );
   }
