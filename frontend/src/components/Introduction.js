45b9eb8c0e0c36cea8e996100a64ff1e2fe85bc6
Michiel Leyman <michiel.leyman@gmail.com>
2019-11-15 15:56:53 +0100

    Content & grammar fixes

--- frontend/src/components/Introduction.js
+++ frontend/src/components/Introduction.js
@@ -4,16 +4,24 @@ import SignUpSection from './SignUpSection';
 const Introduction = () => {
   return (
     <section className="introduction">
-      <img className="introduction-img" src="stitching.png" alt="Stitching your future" title="Stich your experience" />
+      <img
+        className="introduction-img"
+        src="stitching.png"
+        alt="Stitching your future"
+        title="Stich your experience"
+      />
       <div className="introduction-text">
-        <h1>Your companion for your learning journey</h1>
-        <p>How often the world recognizing an individual by her/his formal degree? And what happens with your informal learning journey like attending informal events, writing an article, and so on? Isn't that great if you can show off the worlds both recognitions all in one place? Stitch can help you with that.</p>
+        <h1>The companion for your life-long learning journey</h1>
+        <p>
+          How often does the world recognize an individual by their formal degree? And what happens
+          with your the rest of your (informal) learning journey - such as attending informal
+          events, writing an article, and so on? Wouldn't it be great if you can show off the both
+          worlds of recognition all in one place? Stitch can help you do that.
+        </p>
         <SignUpSection />
       </div>
     </section>
   );
-
 };
-
 
 export default Introduction;
