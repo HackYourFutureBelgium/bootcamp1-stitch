1009c102483da21bceaf4c632ee65517c94c9bd1
abdulsalamalmahdi <getmeips@gmail.com>
2019-11-15 12:27:28 +0100

    a

--- server/user/routes.js
+++ server/user/routes.js
@@ -5,7 +5,7 @@ const controller = require("./controller")
 
 
 router.get("/", controller.findAll);
-router.post("/signUp", controller.signUp);
+router.post("/signup", controller.signUp);
 router.post("/update/:id", controller.update);///need to work 
 router.delete("/:id", controller.postIdDelete);
 
